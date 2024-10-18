12817f10388e661a555ba8490c94c288e698ff39
KunFay99 <kaffayukiffu@gmail.com>
2024-10-18 15:51:40 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,10 +18,5 @@ elif c == "1":
     os.system("pip3 install colorama")
     os.system("pip3 install undetected_chromedriver")
     os.system("pip3 install httpx")
-if os.name == "nt":
-    pass
-else:
-    os.system("wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb")
-    os.system("apt-get install ./google-chrome-stable_current_amd64.deb")
 
 print("Done.")
